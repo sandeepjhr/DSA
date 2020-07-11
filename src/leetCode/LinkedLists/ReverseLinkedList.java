fdb274bc19d5f7519c2cbb1c9309146d7dc79302
sandeepjhr <54406761+sandeepjhr@users.noreply.github.com>
2020-07-12 00:11:27 +0100

    FilesCommitNow

---
+++ src/leetCode/LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,50 @@
+package leetCode.LinkedLists;
+
+public class ReverseLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        ListNode result = reverse(l1);
+        while (result != null) {
+            System.out.print(result.val + ",");
+            result = result.next;
+        }
+
+    }
+
+    private static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+
+        return prev;
+    }
+}
